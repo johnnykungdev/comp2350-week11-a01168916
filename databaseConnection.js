c14ec3f2910d6efed295a411b80773805e8eccd8
Johnny Kung <kjohnathan.engi@gmail.com>
2021-04-14 22:31:39 -0700

    test for heroku

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,31 +1,48 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+
+// const is_heroku = process.env.IS_HEROKU || false;
+
+// const dbConfigHeroku = {
+// 	host: "us-cdbr-east-03.cleardb.com",
+// 	user: "b1ab7fb2ee03bc",
+// 	password: "2a484a2d",
+// 	database: "heroku_3d208ad4bd6f421",
+// 	multipleStatements: false,
+// 	namedPlaceholders: true
+// };
+
+// const dbConfigLocal = {
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "Password",
+// 	database: "lab_example",
+// 	multipleStatements: false,
+// 	namedPlaceholders: true
+// };
+
+// if (is_heroku) {
+// 	var database = mysql.createPool(dbConfigHeroku);
+// }
+// else {
+// 	var database = mysql.createPool(dbConfigLocal);
+// }
+
+// module.exports = database;
+		
+const MongoClient = require("mongodb").MongoClient;
 
 const is_heroku = process.env.IS_HEROKU || false;
 
-const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
+const herokuURI = "mongodb+srv://johnnykung:tzuning615@clusterokewebsite.a4z3c.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "lab_example",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
+const localURI = "mongodb://localhost:27017/?authSource=admin&retryWrites=true&w=majority"
+
+let database;
 
 if (is_heroku) {
-	var database = mysql.createPool(dbConfigHeroku);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+	database = new MongoClient(herokuURI, { useNewUrlParser: true, useUnifiedTopology: true})
+} else {
+	database = new MongoClient(localURI, { useNewUrlParser: true, useUnifiedTopology: true})
 }
 
 module.exports = database;
-		
--- routes/router.js
+++ routes/router.js
@@ -1,22 +1,26 @@
 const router = require('express').Router();
-//const database = include('databaseConnection');
+const database = include('databaseConnection');
+const Joi = require("joi");
 //const dbModel = include('databaseAccessLayer');
 //const dbModel = include('staticData');
 
-const userModel = include('models/web_user');
-const petModel = include('models/pet');
+// const userModel = include('models/web_user');
+// const petModel = include('models/pet');
 
 const crypto = require('crypto');
 const {v4: uuid} = require('uuid');
+const ObjectId = require('mongodb').ObjectId
 
 const passwordPepper = "SeCretPeppa4MySal+";
 
 router.get('/', async (req, res) => {
 	console.log("page hit");
 	try {
-		const users = await userModel.findAll({attributes: ['web_user_id','first_name','last_name','email']}); //{where: {web_user_id: 1}}
+		// const users = await userModel.findAll({attributes: ['web_user_id','first_name','last_name','email']}); //{where: {web_user_id: 1}}
+		const userCollection = database.db("lab_example").collection("users");
+		const users = await userCollection.find().project({first_name: 1, last_name: 1, email: 1, _id: 1}).toArray();
 		if (users === null) {
-			res.render('error', {message: 'Error connecting to MySQL'});
+			res.render('error', {message: 'Error connecting to MongoDB'});
 			console.log("Error connecting to userModel");
 		}
 		else {
@@ -25,7 +29,7 @@ router.get('/', async (req, res) => {
 		}
 	}
 	catch(ex) {
-		res.render('error', {message: 'Error connecting to MySQL'});
+		res.render('error', {message: 'Error connecting to MongoDB'});
 		console.log("Error connecting to MySQL");
 		console.log(ex);
 	}
@@ -55,11 +59,12 @@ router.get('/pets', async (req, res) => {
 
 router.get('/showPets', async (req, res) => {
 	console.log("page hit");
+	const petCollection = database.db("lab_example").collection("pets");
 	try {
 		let userId = req.query.id;
 		const user = await userModel.findByPk(userId); 
 		if (user === null) {
-			res.render('error', {message: 'Error connecting to MySQL'});
+			res.render('error', {message: 'Error connecting to MongoDB'});
 			console.log("Error connecting to userModel");
 		}
 		else {
@@ -72,25 +77,24 @@ router.get('/showPets', async (req, res) => {
 		}
 	}
 	catch(ex) {
-		res.render('error', {message: 'Error connecting to MySQL'});
+		res.render('error', {message: 'Error connecting to MongoDB'});
 		console.log("Error connecting to MySQL");
 		console.log(ex);
 	}
 });
 
 router.get('/deleteUser', async (req, res) => {
+	const userCollection = database.db("lab_example").collection("users")
 	try {
 		console.log("delete user");
+		console.log(req.id)
 
 		let userId = req.query.id;
 		if (userId) {
 			console.log("userId: "+userId);
-			let deleteUser = await userModel.findByPk(userId);
+			let deleteUser = await userCollection.deleteOne({_id: ObjectId(userId)});
 			console.log("deleteUser: ");
 			console.log(deleteUser);
-			if (deleteUser !== null) {
-				await deleteUser.destroy();
-			}
 		}
 		res.redirect("/");
 	}
@@ -102,8 +106,25 @@ router.get('/deleteUser', async (req, res) => {
 });
 
 router.post('/addUser', async (req, res) => {
+	console.log(req.body);
+
+	const schema = Joi.object({
+		password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+		email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+		first_name: Joi.string(),
+		last_name: Joi.string()
+	})
+
+	const validationResult = schema.validate(req.body);
+
+	if (validationResult.error != null) {
+		console.log(validationResult.error);
+		throw validationResult.error;
+	}
+
 	try {
 		console.log("form submit");
+		const userCollection = database.db("lab_example").collection("users");
 
 		const password_salt = crypto.createHash('sha512');
 
@@ -114,7 +135,7 @@ router.post('/addUser', async (req, res) => {
 		password_hash.update(req.body.password+passwordPepper+password_salt);
 
 
-		let newUser = userModel.build(
+		await userCollection.insertOne(
 			{	
 				first_name: req.body.first_name,
 				last_name: req.body.last_name,
@@ -123,11 +144,11 @@ router.post('/addUser', async (req, res) => {
 				password_hash: password_hash.digest('hex')
 			}
 		);
-		await newUser.save();
+	
 		res.redirect("/");
 	}
 	catch(ex) {
-		res.render('error', {message: 'Error connecting to MySQL'});
+		res.render('error', {message: 'Error connecting to MongoDB'});
 		console.log("Error connecting to MySQL");
 		console.log(ex);	
 	}
